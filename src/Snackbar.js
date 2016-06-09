bd5ac3c8b8390ad53ee63fb2d2ac38f00fbb9ea5
Zeno Rocha <hi@zenorocha.com>
2016-06-09 12:06:39 -0700

    Extends metal-alert component

--- src/Snackbar.js
+++ src/Snackbar.js
@@ -1,11 +1,38 @@
 'use strict';
 
 import templates from './Snackbar.soy';
-import Component from 'metal-component';
 import Soy from 'metal-soy';
+import Alert from 'metal-alert';
 
-class Snackbar extends Component {
-}
+class Snackbar extends Alert {}
+
+Snackbar.STATE = {
+	/**
+	 * Spinner indicating.
+	 * @type {boolean}
+	 * @default false
+	 */
+	spinner: {
+		value: false
+	},
+
+	/**
+	 * The CSS classes that should be added to the spinner.
+	 * @type {string}
+	 */
+	spinnerClasses: {
+	},
+
+	/**
+	 * Spinner is marked as done.
+	 * @type {boolean}
+	 * @default false
+	 */
+	spinnerDone: {
+		value: false
+	}
+};
+
 Soy.register(Snackbar, templates);
 
 export default Snackbar;
