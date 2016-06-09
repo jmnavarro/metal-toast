920ba6ca36a3ad136d75d0c01f1e69c152640662
Zeno Rocha <hi@zenorocha.com>
2016-06-09 12:09:39 -0700

    Adds tests to new state config

--- test/Snackbar.js
+++ test/Snackbar.js
@@ -1,9 +1,28 @@
 'use strict';
 
+import dom from 'metal-dom';
 import Snackbar from '../src/Snackbar';
 
 describe('Snackbar', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
+	it('should update spinner as done', function(done) {
+		var component = new Snackbar({
+			spinner: true,
+			spinnerDone: false
+		});
+
+		assert.ok(!dom.hasClass(component.element.querySelector('.alert-spinner'), 'alert-spinner-done'));
+
+		component.once('stateChanged', () => {
+			assert.ok(dom.hasClass(component.element.querySelector('.alert-spinner'), 'alert-spinner-done'));
+
+			component.once('stateChanged', () => {
+				assert.ok(!dom.hasClass(component.element.querySelector('.alert-spinner'), 'alert-spinner-done'));
+				done();
+			});
+
+			component.spinnerDone = false;
+		});
+
+		component.spinnerDone = true;
 	});
 });
